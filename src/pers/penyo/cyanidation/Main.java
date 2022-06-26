abf907c6698ffa81bb7e392171968c09961c3a8d
Penyo <93194508+penyoofficial@users.noreply.github.com>
2022-06-26 12:50:03 +0800

    Delete src/pers/penyo/cyanidation directory

--- src/pers/penyo/cyanidation/Main.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Penyo. All rights reserved.
-
-package pers.penyo.cyanidation;
-
-public class Main {
-    public static void main(String[] args) {
-        new GUI().boot("1.0.2");
-    }
-}
