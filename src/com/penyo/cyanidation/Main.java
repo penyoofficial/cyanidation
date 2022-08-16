c6a98b50f8bdbfdffd5db89471826b7150e82c80
Penyo <93194508+penyoofficial@users.noreply.github.com>
2022-08-16 11:38:28 +0800

    Add files via upload

---
+++ src/com/penyo/cyanidation/Main.java
@@ -0,0 +1,26 @@
+/* 
+ * Copyright (c) Penyo. All rights reserved.
+ */
+
+package com.penyo.cyanidation;
+
+import com.penyo.cyanidation.basics.GUI;
+
+/**
+ * 欢迎使用 {@code Cyanidation} ——目标是打造最轻量化的课程表！
+ * 
+ * <p>
+ * 在用过市面上那么多的课程表之后，我发现竟然找不到一款是纯粹的。
+ * 而我只想用课程表功能。
+ * 额外的广告、脑残一样的交互逻辑，让人感到非常的不爽。
+ * 本着这样的愿景，Penyo启动了 {@code Cyanidation} 计划。
+ * 也许它在初期还不太完美：不够好用、不够美观......
+ * 但是咱是有信心把它做好的——尽管这可能需要多年的技术积累。
+ * 
+ * @author Penyo
+ */
+public class Main {
+    public static void main(String[] args) {
+        new GUI("1.0.2.22228");
+    }
+}
---
+++ src/com/penyo/cyanidation/attachment/PressureDetector.java
@@ -0,0 +1,9 @@
+/* 
+ * Copyright (c) Penyo. All rights reserved.
+ */
+
+package com.penyo.cyanidation.attachment;
+
+public class PressureDetector {
+    int[][] pressure = new int[7][5];
+}
