08243f16cf929dd7c76e984550a066e57b2ed402
Penyo <penyoofficial@outlook.com>
2023-06-28 10:37:24 +0800

    23w26b
    - 前端框架设计完成。
    - 配合UX设计补充了数据结构设计。

--- defination.js
+++ defination.js
@@ -1,31 +1,31 @@
 /**
  * 时间多态是科目在不同时间所呈现的不同状态。
-*/
+ */
 class TimeMorph {
     /** 
      * 该时间多态在一天中的时间段
      * @type {number[]} 
-    */
+     */
     time
     /** 
      * 该时间多态在一周中的天数
      * @type {number} 
-    */
+     */
     weekday
     /**
      * 该时间多态在一学期中的教学周段
      * @type {number[]} 
-    */
+     */
     week
     /**
      * 该时间多态的教学位置
      * @type {string} 
-    */
+     */
     place
     /**
      * 该时间多态的授课教师
      * @type {string} 
-    */
+     */
     teacher
 
     /**
@@ -37,7 +37,7 @@ class TimeMorph {
      * @param {number} weekUB 教学周段上界
      * @param {string} place 教学位置
      * @param {string} teacher 授课教师
-    */
+     */
     constructor(timeLB, timeUB, weekday, weekLB, weekUB, place, teacher) {
         this.time = [timeLB, timeUB]
         this.weekday = weekday
@@ -49,23 +49,23 @@ class TimeMorph {
 
 /**
  * 科目是对现实世界学科的抽象。
-*/
+ */
 class Subject {
     /** 
      * 科目名称
      * @type {string} 
-    */
+     */
     name
     /**
      * 科目时间多态
      * @type {TimeMorph[]} 
-    */
+     */
     timeMorph = []
 
     /**
      * 构造科目实例。
      * @param {string} name 科目名称
-    */
+     */
     constructor(name) {
         this.name = name
     }
@@ -79,7 +79,7 @@ class Subject {
      * @param {number} weekUB 教学周段上界
      * @param {string} place 教学位置
      * @param {string} teacher 授课教师
-    */
+     */
     addTimeMorph(timeLB, timeUB, weekday, weekLB, weekUB, place, teacher) {
         this.timeMorph.push(new TimeMorph(timeLB, timeUB, weekday, weekLB, weekUB, place, teacher))
     }
@@ -88,7 +88,7 @@ class Subject {
      * 返回两个科目是否相同。
      * @param {Subject} anotherSubject
      * @returns {boolean}
-    */
+     */
     equals(anotherSubject) {
         return this.name == anotherSubject.name
     }
@@ -96,29 +96,58 @@ class Subject {
 
 /**
  * 学期是科目在一段时间内离散状态的集合。
-*/
+ */
 class Term {
     /**
      * 开始日期
      * @type {Date}
-    */
+     */
     startDate
     /**
      * 科目
      * @type {Subject[]}
-    */
+     */
     subjects = []
 
     /**
+     * 构造学期实例。可产生空白实例或依据 JSON 结构化。
+     * @param {string} json 序列化的 JSON 字符串
+     */
+    constructor(json) {
+        if (json) {
+            try {
+                const data = JSON.parse(json)
+                this.startDate = new Date(data.startDate)
+                for (let subjectData of data.subjects) {
+                    let subject = new Subject(subjectData.name)
+                    for (let timeMorphData of subjectData.timeMorph) {
+                        let timeMorph = new TimeMorph(
+                            parseInt(timeMorphData.time[0]),
+                            parseInt(timeMorphData.time[1]),
+                            parseInt(timeMorphData.weekday),
+                            parseInt(timeMorphData.week[0]),
+                            parseInt(timeMorphData.week[1]),
+                            timeMorphData.place,
+                            timeMorphData.teacher
+                        )
+                        subject.timeMorph.push(timeMorph)
+                    }
+                    this.subjects.push(subject)
+                }
+            } catch (e) { }
+        }
+    }
+
+    /**
      * 获取学期实例。
-     * *注意，该方法应当取代构造器，除非用户对类结构了如指掌。*
+     * *注意，该方法应当取代构造器来被使用，除非用户对类结构了如指掌。*
      * @param {string} mode 文件模式（学校代号）
      * @param {file} file 文件实体
      * @returns {Term}
-    */
+     */
     static async getInstance(mode, file) {
         const getWeekdayNumber = (weekday) => {
-            const weekdays = {
+            return {
                 '星期一': 1,
                 '星期二': 2,
                 '星期三': 3,
@@ -126,8 +155,7 @@ class Term {
                 '星期五': 5,
                 '星期六': 6,
                 '星期日': 7
-            }
-            return weekdays[weekday]
+            }[weekday]
         }
         let t = new Term()
         switch (mode) {
@@ -167,7 +195,7 @@ class Term {
      * @param {file} file 
      * @param {string} range 
      * @return {object}
-    */
+     */
     parseExcel(file, range) {
         return new Promise((resolve, reject) => {
             const reader = new FileReader()
@@ -198,7 +226,7 @@ class Term {
      * 检索学期对象内是否有指定名称的科目。
      * @param {string} name 科目名称
      * @returns {boolean}
-    */
+     */
     hasSubject(name) {
         let flag = false
         this.subjects.forEach(s => {
@@ -212,7 +240,7 @@ class Term {
      * 获取指定名称对应科目的引用。
      * @param {string} name 科目名称
      * @returns {Subject}
-    */
+     */
     getSubject(name) {
         let flag = null
         this.subjects.forEach(s => {
@@ -226,7 +254,7 @@ class Term {
      * 获得特定时间的切片。
      * @param {Date} moment 时刻（天）
      * @returns {Cut[]}
-    */
+     */
     getTodayCuts(moment) {
         let cuts = []
         const weekOfTerm = Math.floor((moment - this.startDate) / 1000 / 60 / 60 / 24 / 7) + 1
@@ -244,6 +272,18 @@ class Term {
     }
 
     /**
+     * 获取特定时间在学期中的周数。
+     * @param {Date} moment 时刻（天）
+     * @returns {number}
+    */
+    getWeek(moment) {
+        const week = Math.floor((moment - this.startDate) / 1000 / 60 / 60 / 24 / 7) + 1
+        if (week > 0)
+            return week
+        return week - 1
+    }
+
+    /**
      * 添加科目切片到学期中。
      * @param {string} name 科目名称
      * @param {number} timeLB 时间段下界
@@ -253,7 +293,7 @@ class Term {
      * @param {number} weekUB 教学周段上界
      * @param {string} place 教学位置
      * @param {string} teacher 授课教师
-    */
+     */
     addSubjectCut(name, timeLB, timeUB, weekday, weekLB, weekUB, place, teacher) {
         if (!this.hasSubject(name))
             this.subjects.push(new Subject(name))
@@ -263,27 +303,27 @@ class Term {
 
 /**
  * 学期在特定时间的切片，时间多态坍塌成为确定态。
-*/
+ */
 class Cut {
     /**
      * 科目名称
      * @type {string}
-    */
+     */
     name
     /**
      * 科目时间段
      * @type {number[]}
-    */
+     */
     time
     /**
      * 教学位置
      * @type {string}
-    */
+     */
     place
     /**
      * 授课教师
      * @type {string}
-    */
+     */
     teacher
 
     /**
@@ -293,7 +333,7 @@ class Cut {
      * @param {number} timeUB 时间段上界
      * @param {string} place 教学位置
      * @param {string} teacher 授课教师
-    */
+     */
     constructor(name, timeLB, timeUB, place, teacher) {
         this.name = name
         this.time = [timeLB, timeUB]
--- main.js
+++ main.js
@@ -1,17 +1,85 @@
-function renderTest(time, cuts) {
-    let today = document.querySelector("#today")
-    today.innerHTML = time + "<br>"
-    cuts.forEach(c => {
-        today.innerHTML +=
-            c.time[0] + "-" + c.time[1] + " " + c.name + "：" + c.place + "<br>"
-    })
+/**
+ * 存储学期对象到本地。
+*/
+async function setData() {
+    const mode = document.querySelector("#university-selector").value
+    const term = await Term.getInstance(mode, document.querySelector("#excel-selector").files[0])
+    localStorage.setItem("cy-data", JSON.stringify(term))
 }
 
-document.querySelector('input[type="file"]').onchange = async function (e) {
-    const term = await Term.getInstance("HFUU", e.target.files[0])
-    const cuts = term.getTodayCuts(new Date("2023-09-05"))
-    renderTest(new Date("2023-09-05"), cuts)
+/**
+ * 获取存储在本地的学期对象。
+ * @returns {Term | null}
+*/
+function getData() {
+    const d = localStorage.getItem("cy-data")
+    if (d)
+        return new Term(d)
+    return null
+}
+
+/**
+ * 渲染指定时间内，学期中可用的切片。
+ * @param {Date} time 指定的时间
+*/
+function render(time) {
+    const term = getData()
+    const cutsToday = term.getTodayCuts(time)
+    const timeByd = new Date(time.getTime() + 24 * 60 * 60 * 1000)
+    const cutsTomorrow = term.getTodayCuts(timeByd)
+    let today = document.querySelector("#today")
+    let tomorrow = document.querySelector("#tomorrow")
+    const getChinese = (num) => {
+        return {
+            1: "一",
+            2: "二",
+            3: "三",
+            4: "四",
+            5: "五",
+            6: "六",
+            0: "日"
+        }[num]
+    }
+    const judgeWeekend = (day) => {
+        if (day == 6 || day == 0)
+            return "weekend"
+        return ""
+    }
+    const judgeWeek = (time) => {
+        const week = term.getWeek(time)
+        if (week > 0)
+            return `第${week}周 `
+        return `开学前${-week}周 `
+    }
+    today.innerHTML = `
+        <div class="date ${judgeWeekend(time.getDay())}">
+            ${time.getMonth() + 1}月${time.getDate()}日， ${judgeWeek(time)}周${getChinese(time.getDay())}
+        </div>
+        <div class="unit-title">今天</div>
+    `
+    tomorrow.innerHTML = `
+        <div class="date ${judgeWeekend(timeByd.getDay())}">
+            ${timeByd.getMonth() + 1}月${timeByd.getDate()}日，${judgeWeek(timeByd)}周${getChinese(timeByd.getDay())}
+        </div>
+        <div class="unit-title">明天</div>
+    `
+    if (cutsToday.length)
+        cutsToday.forEach(c => {
+            today.innerHTML += `
+                <div class="cut">${c.time[0]}-${c.time[1]}\t${c.name}：${c.place}（${c.teacher}）</div>
+            `
+        })
+    else
+        today.innerHTML += `<div class="cut">空闲！</div>`
+    if (cutsTomorrow.length)
+        cutsTomorrow.forEach(c => {
+            tomorrow.innerHTML += `
+                <div class="cut">${c.time[0]}-${c.time[1]}\t${c.name}：${c.place}（${c.teacher}）</div>
+            `
+        })
+    else
+        tomorrow.innerHTML += `<div class="cut">空闲！</div>`
     setInterval(() => {
-        renderTest(new Date("2023-09-05"), cuts)
+        render(new Date())
     }, 1800000)
 }
